bdfd48f307b79b27170f61414ca935e086c36264
David Gil <64413374+davdgil@users.noreply.github.com>
2024-02-27 10:55:07 +0100

    jsd

--- mvc/controllers/tracks.js
+++ mvc/controllers/tracks.js
@@ -1,22 +1,55 @@
 const { tracksModel } = require('../models')
+const { matchedData } = require('express-validator')
+const { handleHttpError } = require('../utils/handleError')
+
 const getItems = async (req, res) => {
-    const data = await tracksModel.find({})
-    res.send(data)
+
+    try {
+        const data = await tracksModel.find({})
+        res.send(data)
+    } catch (err) {
+        handleHttpError(res, 'ERROR_GET_ITEMS', 403)
+    }
+
 }
 const createItem = async (req, res) => {
-    const { body } = req
-    const data = await
-    tracksModel.create(body)
-    res.send(data)
+    try {
+        const { body } = matchedData(req) // el dato filtrado por el modelo (probar con body = req)
+        const data = await
+            tracksModel.create(body)
+        res.send(data)
+    } catch (err) {
+        handleHttpError(res, 'ERROR_CREATE_ITEMS')
+    }
+
 }
 
 
-const getItem = (req, res) => {
+const getItem = async (req, res) => {
+
+    try {
+        const { id } = matchedData(req)
+        console.log(id)
+        const track = await tracksModel.findById(id)
+        if (!track) {
+            throw err;
+        }
+        res.send(track)
+
+    } catch (err) {
+        handleHttpError(res, 'ERROR_GET_ITEM', 404)
+    }
 
 }
 
-const updateItem = (req, res) => {
-    
+const updateItem = async(req, res) => {
+    try {
+        const { id, ...body } = matchedData(req); // Extrae el id y el resto lo asigna a la constante body
+        const data = await tracksModel.findOneAndUpdate(id, body);
+        res.send(data)
+    } catch (err) {
+        handleHttpError(res, 'ERROR_UPDATE_ITEMS')
+    }
 }
 
 const deleteItem = (req, res) => {
--- mvc/routes/tracks.js
+++ mvc/routes/tracks.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { getItems, getItem, createItem } = require("../controllers/tracks")
+const { getItems, getItem, createItem, updateItem } = require("../controllers/tracks")
+const { validatorCreateItem, validatorUpdateItem, validatorGetItem } = require("../validators/tracks")
+const customHeader = require("../middleware/customHeader")
 
 router.get("/", getItems)
-router.post("/", createItem)
-router.get("/:id", getItem)
+router.post("/", validatorCreateItem, customHeader, createItem)
+router.get("/:id",validatorGetItem, getItem)
+router.put("/:id", validatorUpdateItem, customHeader, updateItem);
+
 
 module.exports = router
