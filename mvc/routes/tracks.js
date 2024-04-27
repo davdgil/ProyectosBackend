0ccb1b05f3d5936491a560e4e4fde6ebcbe0ede1
David Gil <64413374+davdgil@users.noreply.github.com>
2024-04-27 06:00:19 +0200

    z

--- mvc/routes/tracks.js
+++ mvc/routes/tracks.js
@@ -3,9 +3,14 @@ const router = express.Router()
 const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/tracks")
 const { validatorCreateItem, validatorGetItem } = require("../validators/tracks")
 const customHeader = require("../middleware/customHeader")
+const authMiddleware = require("../middleware/session")
+const checkRol = require("../middleware/rol")
 
-router.get("/", getItems)
-router.post("/", validatorCreateItem, createItem)
+
+
+
+router.get("/", authMiddleware, getItems)
+router.post("/", authMiddleware, checkRol(["admin"]), validatorCreateItem, createItem)
 router.get("/:id",validatorGetItem, getItem)
 router.put("/:id", validatorGetItem, validatorCreateItem, updateItem);
 router.delete("/:id", validatorGetItem, deleteItem)
